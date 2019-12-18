e198065984b78bb27ee9aa88a3ecac2be6916afd
yong jun jo <t08361@naver.com>
2019-12-18 12:18:06 +0900

    run adroid

--- sleep_term_app/App.js
+++ sleep_term_app/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { AdMobBanner } from 'react-native-admob'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>숙면의 시작과 끝</Text>
+      <AdMobBanner
+        adSize="fullBanner"
+        adUnitID="your-admob-unit-id"
+        testDevices={[AdMobBanner.simulatorId]}
+        onAdFailedToLoad={error => console.error(error)}
+      />
+      <View>
+        <TouchableOpacity>
+          <Text style={styles.button_time}>30분후</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text style={styles.button_time}>60분후</Text>
+        </TouchableOpacity>    
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text style={styles.button_time}>90분후</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +39,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    marginBottom: 220,
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontFamily:'Roboto'
+  },
+  button_time: {
+    color:'white',
+    backgroundColor: '#FFC37B',
+    padding: 26,
+    borderRadius: 23,
+    margin: 5,
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
